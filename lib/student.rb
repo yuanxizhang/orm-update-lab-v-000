90955998197ec76ba47832d5b4d5652e94a7fac3
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-22 03:54:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,6 +66,6 @@ class Student
       SET name = ?, grade = ?
       WHERE id = ?;
       SQL
-    row = DB[:conn].execute(sql, self.name, self.grade, self.id)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 end
